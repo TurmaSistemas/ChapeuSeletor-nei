b0805e2d801751e9380ad0f2b084c6066d484acc
JeanCts <51141716+JeanCts@users.noreply.github.com>
2019-05-28 21:52:35 -0300

    Update dao.class.php

--- ChapeuSeletor/Dal/dao.class.php
+++ ChapeuSeletor/Dal/dao.class.php
@@ -7,7 +7,7 @@ class Dao
 	public function __construct()
 	{
 		require_once(dirname(__FILE__).
-		"/../../../../protec/conecta_site.php");
+		"/../../../../protec/conecta_chapeu.php");
 		$this->con = conecta_site();
 	}
 
