3184cde2bdd2d9a68cfa2c65b4d8375d81460e9e
Jocimeire <44402788+Jocimeire@users.noreply.github.com>
2019-05-31 16:24:36 -0300

    Update carregaAlternativa.ajax.php

--- ChapeuSeletor/Controller/carregaAlternativa.ajax.php
+++ ChapeuSeletor/Controller/carregaAlternativa.ajax.php
@@ -5,7 +5,7 @@ require_once(dirname(__FILE__)."/../view/alternativa.class.php");
 
 $dao= new $dao();
 
-id_alt = ??????;//devo fazer um rand? ou comparar com o id da pergunta?
+id_alt = rand(0, 25);
 
 $alternativa = $dao->carrega_alternativa($id_alt); //Carrega uma alternativa aleatoria
 $vAlternativa = new alternativa($alternativa); //Manda para a view o objeto alternativa
