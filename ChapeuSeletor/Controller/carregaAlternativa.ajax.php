53f4e2433ac1d7bc683db859f07f07b2a796fbab
Jocimeire <44402788+Jocimeire@users.noreply.github.com>
2019-05-31 16:13:41 -0300

    Update carregaAlternativa.ajax.php

--- ChapeuSeletor/Controller/carregaAlternativa.ajax.php
+++ ChapeuSeletor/Controller/carregaAlternativa.ajax.php
@@ -1,5 +1,16 @@
 <?php
+
 require_once(dirname(__FILE__)."/../dal/dao.class.php");
 require_once(dirname(__FILE__)."/../view/alternativa.class.php");
 
+$dao= new $dao();
+
+id_alt = rand(0,25);
+
+$alternativa = $dao->carrega_alternativa($id_alt); //Carrega uma alternativa aleatoria
+$vAlternativa = new alternativa($alternativa); //Manda para a view o objeto alternativa
+$vAlternativa->draw(); //Exibe
+
 ?>
+
+
